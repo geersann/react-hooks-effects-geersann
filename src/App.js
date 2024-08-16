1a8da2d8c4a2915acf5d3de18e5cadcd2b1d156b
geersann <148795588+geersann@users.noreply.github.com>
2024-08-16 00:26:01 +0000

    add localStorage to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import {useState, useEffect} from 'react';
 import './App.css';
 
 export default function App() {
+  const [value, setValue] = useState (() => {
+    return localStorage.getItem('appData') || '';
+  })
 
+  useEffect(() => {
+    localStorage.setItem('appData', value)
+  }, [value]);
   return (
     <div>
-      React Marathon, appData: <input size='5'></input>
+      React Marathon, appData: <input value={value}
+      onChange={(e) => setValue(e.target.value)}
+      />
     </div>
   );
 }
